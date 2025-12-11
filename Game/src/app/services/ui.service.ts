50ddf9a053ece6e4abd6775f4bdd03738156d9e9
TimoStricker <Timo.Stricker@student.uni-siegen.de>
2025-12-11 18:54:06 +0100

    PopUp Position

--- Game/src/app/services/ui.service.ts
+++ Game/src/app/services/ui.service.ts
@@ -176,8 +176,10 @@ export class UIService {
     };
 
     const center = popupConfig.width / 2;
-    const x = machine.x + popupConfig.xOffset;
-    const y = machine.y * Math.cos(this._angle) + popupConfig.yOffset;
+    // const x = machine.x + popupConfig.xOffset;
+    // const y = machine.y * Math.cos(this._angle) + popupConfig.yOffset;
+    const x = 50;
+    const y = 650
 
     this.lastMachinePopupRect = {
       x: x - 10, y: y - 10, width: popupConfig.width + 20, height: popupConfig.height + 20
