383e61e9c7ec64770236a97293132e23ab50128c
Ha-Khoa <khoab30@gmail.com>
2025-12-28 11:36:34 +0700

    Apply stash changes and finalize merge from origin/Khoa

--- Game/src/app/models/preProcess/prep-machine-manager.spec.ts
+++ Game/src/app/models/preProcess/prep-machine-manager.spec.ts
@@ -1,7 +1,8 @@
 import { PrepMachineManager } from './prep-machine-manager';
-
+import { Gamefield } from '../gamefield/gamefield';
 describe('PrepMachineManager', () => {
   it('should create an instance', () => {
-    expect(new PrepMachineManager()).toBeTruthy();
+    const gamefield = new Gamefield();
+    expect(new PrepMachineManager(gamefield)).toBeTruthy();
   });
 });
