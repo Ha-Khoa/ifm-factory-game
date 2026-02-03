331b7ee53102f0eafa95aa6e08a6a0fd8be3defa
David <david.iljuchin@gmail.com>
2026-02-03 22:25:33 +0100

    bug fixes

--- Game/src/app/models/product/products.ts
+++ Game/src/app/models/product/products.ts
@@ -123,7 +123,7 @@ export class Products {
     for (let interactionObject of objects) {
 
       collision = Collision.checkCollision(new Hitbox(product.position, product.size, product.size), new Hitbox(new Coordinates(interactionObject.x, interactionObject.y), interactionObject.width, interactionObject.height))
-      if (collision && !interactionObject.name.startsWith("conveyor") && !interactionObject.name.startsWith("Machine")) {
+      if (collision && interactionObject.name.startsWith("table") ) {
 
         if (!this.checkItemOnTable(interactionObject, product)) {
           collisions.push(interactionObject)
